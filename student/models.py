f7a59b40d19aee481a1487ddcec1b72506e611af
MBR-ACS <bhagyaraju.marapatla@alladicloudsolutions.com>
2025-10-07 11:19:52 +0530

    initial

---
+++ student/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50, null=False)
+    email = models.EmailField(null=False)
+    age = models.IntegerField(null=True)
+    phone = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=25, null=False)
